530e4e272ea64e6b18ab757477158c58bafd6958
sparrow1w <antexas@gmail.com>
2018-01-25 02:11:03 +0200

    added responsive design, completed form validation

---
+++ js/form.js
@@ -0,0 +1,38 @@
+
+function onFormSubmit() {
+    var firstname = document.forms['login-form']['firstname'].value;
+    var lastname = document.forms['login-form']['lastname'].value;
+    var message = document.getElementById("myTextarea").value;
+    var elementas = document.getElementById('p1');
+
+
+
+    var numb = /^[0-9]+$/;
+    if (firstname == "") {
+        elementas.innerHTML = "Please enter Your first name!";
+        elementas.className = "red-alert";
+        return false;
+
+    }else if (lastname == "") {
+        elementas.innerHTML = "Please enter Your last name!";
+        elementas.className = "red-alert";
+        return false;
+        
+    }else if (firstname.match(numb) || lastname.match(numb) ) {
+        elementas.innerHTML = "First name and last name must have alphabet characters only!";
+        elementas.className = "red-alert";
+        return false;
+    
+    }else if (message == "") {
+        elementas.innerHTML  = "Please write Your message!";
+        elementas.className = "red-alert";
+        return false;
+    }else {
+        elementas.innerHTML = "Thank You for Your message!";
+        elementas.className = "green-alert";
+        return true;
+
+    }
+
+
+}
